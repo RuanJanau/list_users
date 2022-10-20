c1bd119d05009960f41f7ae7ea6bd908adb39f73
Ruan Janaú <rhuanjanau531@gmail.com>
2022-10-20 20:48:38 -0300

    feat: created dummyUsers

---
+++ lib/src/data/dummy_user.dart
@@ -0,0 +1,32 @@
+import '../models/user.dart';
+
+const Map<String, User> dummyUsers = {
+  "1": User(
+    id: " 1",
+    name: "Jardiano",
+    email: "Jardiano.almeida@gmail.com",
+    avatarUrl:
+        "https://cdn.pixabay.com/photo/2013/07/12/16/34/pumpkin-151180_1280.png",
+  ),
+  "2": User(
+    id: "2",
+    name: "Lia",
+    email: "liaaredes@gmail.com",
+    avatarUrl:
+        "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295431_1280.png",
+  ),
+  "3": User(
+    id: " 3",
+    name: "Ruan",
+    email: "ruanjanau@gmail.com",
+    avatarUrl:
+        "https://cdn.pixabay.com/photo/2017/01/31/15/12/avatar-2024924_1280.png",
+  ),
+  "4": User(
+    id: "4",
+    name: "Luan",
+    email: "luanbarroso@gmail.com",
+    avatarUrl:
+        "https://cdn.pixabay.com/photo/2013/07/12/19/14/angry-154378_1280.png",
+  ),
+};
